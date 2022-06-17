eadb6f3428f03f8536647f78cc1f28fff9318e22
elmasta <valentinrobin1@gmail.com>
2022-06-17 09:44:58 +0200

    html and css merged, view almost finished

--- app/views.py
+++ app/views.py
@@ -17,21 +17,22 @@ def index():
 @app.route("/process", methods=["POST"])
 def process():
 
+    datas = []
     if request.form["sinon"]:
         print(request.form["sinon"])
         results = Producteurs.query.filter(Producteurs.cat.contains(request.form["sinon"]), Producteurs.dept.contains("Seine-Maritime"))
-        for result in results:
-            print(result.name)
     else:
+        results = Producteurs.query.filter(Producteurs.cat.contains(request.form["chooseYourFarm"]), Producteurs.dept.contains("Seine-Maritime"))
         print(request.form["chooseYourFarm"])
-    
-    return render_template("index.html")
-
-    # items = Content.query.filter_by(style=request.form[]).order_by(Content.name).all()
-
-    # retour json d'une liste provenant de la BDD
-    # return jsonify({"lat": items["lat"],
-    #                 "longi": items["lng"]})
+    for result in results:
+        addr = result.addr + " " + result.cp + " " + result.ville + " " + result.dept
+        datas.append({"name" : result.name,
+                      "cat" : result.cat,
+                      "addr": addr,
+                      "contact": result.contact,
+                      "lat": result.lat,
+                      "lon": result.lon})
+    return jsonify(datas)
 
 
 if __name__ == "__main__":
